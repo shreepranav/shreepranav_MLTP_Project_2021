0fd905edd5a4061b49a4647c2c74125440a6333f
Shreepranav Varma <shreepranav@gmail.com>
2021-05-24 10:34:41 +0530

    v1

---
+++ tests/test_cases.py
@@ -0,0 +1,17 @@
+from z3 import *
+
+x1, x2, x3, x4, x5, x6, x7 = Reals('x1 x2 x3 x4 x5 x6 x7')
+f1, f2, f3 = Reals('f1 f2 f3')
+g1, g2 = Reals('g1 g2')
+h1, h2 = Reals('h1 h2')
+
+formula1 = And(x1==x2, x2==x3, x1!=x3)
+formula2 = Or(x1==x2, x2==x3, x3==x4, x4!=x5, x5==x6, x6==x7)
+formula3 = And(x1==x2, x4==x5, x5!=x1, x2==x3, f1!=f3, g1==g2, h1==h2)
+formula4 = And(Or(x1==x2, x3!=x4), Or(x1==x4, x2==x4))
+formula5 = Implies(And(x1==x2, x2==x3), x1==x3)
+formula6 = Implies(Not(And(x1==x2, x2==x3)), Or(Not(x1==x2), Not(x2==x3)))
+formula7 = And(Or(x1==x2, x2==x3, x3==x4), Or(x2==x1, x1==x4), Not(Or(x3==x3, x2==x3)))
+formula8 = And(x1==x2, x2==x3, x3==x4, x4==f1, f1==f2, f2==g1, g1==g2, g2==h1, h1==h2, h2!=x2)
+formula9 = And(x1==x2, x1==x3, Or(x1!=x2, f1==f2), Or(x2!=x3, f2==f3), f1!=f3)
+formula10 = And(x1==x2, Or(x2==x3, f2==f3), f1!=f3, Implies(x1==x3, f1==f3), Implies(x2==x3, f2==f3))
---
+++ tests/test_functions.py
@@ -0,0 +1,99 @@
+# This module contains functions that will test each of the three functions bryant, dpllt and dnf-eg
+from equality_logic_algorithms.bryant import bryant
+from equality_logic_algorithms.dpllt import dpllt
+from equality_logic_algorithms.dnf_eg import dnf_eg
+
+from test_cases import *
+
+# Testing Bryant: Ten test cases of the form test_formula*_bryant()
+def test_formula1_bryant():
+    assert bryant(formula1) == z3.unsat
+
+def test_formula2_bryant():
+    assert bryant(formula2) == z3.sat
+
+def test_formula3_bryant():
+    assert bryant(formula3) == z3.sat
+
+def test_formula4_bryant():
+    assert bryant(formula4) == z3.sat
+
+def test_formula5_bryant():
+    assert bryant(formula5) == z3.sat
+
+def test_formula6_bryant():
+    assert bryant(formula6) == z3.sat
+
+def test_formula7_bryant():
+    assert bryant(formula7) == z3.unsat
+
+def test_formula8_bryant():
+    assert bryant(formula8) == z3.unsat
+
+def test_formula9_bryant():
+    assert bryant(formula9) == z3.unsat
+
+def test_formula10_bryant():
+    assert bryant(formula10) == z3.sat
+
+# Testing DPLL(T): Ten test cases of the form test_formula*_dpllt()
+def test_formula1_dpllt():
+    assert dpllt(formula1) == z3.unsat
+
+def test_formula2_dpllt():
+    assert dpllt(formula2) == z3.sat
+
+def test_formula3_dpllt():
+    assert dpllt(formula3) == z3.sat
+
+def test_formula4_dpllt():
+    assert dpllt(formula4) == z3.sat
+
+def test_formula5_dpllt():
+    assert dpllt(formula5) == z3.sat
+
+def test_formula6_dpllt():
+    assert dpllt(formula6) == z3.sat
+
+def test_formula7_dpllt():
+    assert dpllt(formula7) == z3.unsat
+
+def test_formula8_dpllt():
+    assert dpllt(formula8) == z3.unsat
+
+def test_formula9_dpllt():
+    assert dpllt(formula9) == z3.unsat
+
+def test_formula10_dpllt():
+    assert dpllt(formula10) == z3.sat
+
+# Testing DNF+Equality Graph: Ten test cases of the form test_formula*_dnf_eg()
+def test_formula1_dnf_eg():
+    assert dnf_eg(formula1) == z3.unsat
+
+def test_formula2_dnf_eg():
+    assert dnf_eg(formula2) == z3.sat
+
+def test_formula3_dnf_eg():
+    assert dnf_eg(formula3) == z3.sat
+
+def test_formula4_dnf_eg():
+    assert dnf_eg(formula4) == z3.sat
+
+def test_formula5_dnf_eg():
+    assert dnf_eg(formula5) == z3.sat
+
+def test_formula6_dnf_eg():
+    assert dnf_eg(formula6) == z3.sat
+
+def test_formula7_dnf_eg():
+    assert dnf_eg(formula7) == z3.unsat
+
+def test_formula8_dnf_eg():
+    assert dnf_eg(formula8) == z3.unsat
+
+def test_formula9_dnf_eg():
+    assert dnf_eg(formula9) == z3.unsat
+
+def test_formula10_dnf_eg():
+    assert dnf_eg(formula10) == z3.sat
